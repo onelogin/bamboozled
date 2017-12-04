ec942d6f3e772cd1299fb2ee9af5edd01b2a5ca3
Oleh Maikovych <omaik@softserveinc.com>
2017-12-04 13:57:00 +0200

    simplify tables endpoint

--- lib/bamboozled/api/meta.rb
+++ lib/bamboozled/api/meta.rb
@@ -2,12 +2,15 @@ module Bamboozled
   module API
     class Meta < Base
 
-      [:field, :table, :list, :user].each do |action|
+      [:field, :list, :user].each do |action|
         define_method("#{action}s") do
           request(:get, "meta/#{action}s")
         end
       end
 
+      def tables
+        request(:get, 'meta/tables')[:tables][:table]
+      end
     end
   end
 end
